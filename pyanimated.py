9d5be5793ca10f091bf5bbd298e059d09ffd09b1
Erik Campobadal Forés <ConsoleTVs@gmail.com>
2020-06-02 19:27:47 +0200

    Simplify the code

--- pyanimated.py
+++ pyanimated.py
@@ -1,41 +1,34 @@
-from sys import *
-from time import *
-
-
+from sys import stdout
+from time import sleep
 
 class PyAnimated:
-	"""Intro Animation Class
 	"""
-
-
+	Intro Animation Class
+	"""
+	
 	def intro(self):
-		"""Return intro text.
 		"""
+		Prints the intro text.
+		"""
+		self.textAnimated("<===== PyAnimated, your animated Python Library. =====>", 0.05)
 
-		introText = "<===== PyAnimated, your animated Python Library. =====>"
-
-		for i in introText:
-			stdout.write(i), stdout.flush()
-			sleep(0.05)
-			
+	def _outputText(self, text, speed, newLine):
+		"""
+		Prints the given text with the indicated speed (sec) and appends a new line if needed.
+		"""
+		for i in text:
+			stdout.write(f"{i}{'\n' if newLine else ''}")
+	     		stdout.flush()
+			sleep(speed)
 
 	def textAnimated(self, text, speed):
-		"""Return customed text with indicated speed(sec)
 		"""
-
-		for i in text:
-			stdout.write(i), stdout.flush()
-			sleep(speed)
-
+		Prints the given text with the indicated speed(sec)
+		"""
+		self._outputText(text, speed, false)
 
 	def verticalTextAnimated(self, text, speed):
-		"""Return customed text with indicated speed(sec) in vertical
 		"""
-
-		for i in text:
-			print(i)
-			sleep(speed)
-
-
-
-#if __name__ == "__main__":
+		Prints the given text with the indicated speed(sec) in vertical
+		"""
+		self._outputText(text, speed, true)
